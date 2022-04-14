c09202a41bc7e4434a6c69bcd9b14594295f3e67
EliasEF <eliasef0305@gmail.com>
2022-04-13 22:49:38 -0300

    exercicios do bloco 5 dia 1 - seletores do javascript com o DOM

---
+++ fundamentos/bloco-5-javascript-DOM-eventos-web-storage/dia-1-DOM-e-seletores/seletores-de-elementos/script2.js
@@ -0,0 +1,7 @@
+let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual")
+
+for (let i = 0; i < pilotosDeF1.length; i++) {
+    pilotosDeF1[i].innerText = "Lewis Hemilton"
+}
+
+document.getElementsByClassName("teste")[0].style.background = 'red'
