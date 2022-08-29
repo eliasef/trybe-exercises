c886f71343e1c80e45d2eaabc3e974585fcd9cf7
EliasEF <eliasef0305@gmail.com>
2022-08-29 18:02:33 -0300

    eexercicio doguinho

---
+++ 2.front-end/bloco-15-redux/dia-4-actions-assincronas/doguinhos-app/src/store/index.js
@@ -0,0 +1,71 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const GET_IMAGE = 'GET_IMAGE'
+const REQUEST_IMAGE = "REQUEST_IMAGE";
+const FAILED_REQUEST = "FAILED_REQUEST";
+
+function actionGetImage(json) {
+    return {
+        type: GET_IMAGE,
+        payload: json.message,
+    }
+}
+
+function actionRequestDog() {
+    return {
+        type: REQUEST_IMAGE,
+    }
+}
+
+function actionFailedRequest(error) {
+    return {
+        type: FAILED_REQUEST,
+        payload: error,
+    }
+}
+
+export function actionFetchDog() {
+    return (dispatch) => {
+        dispatch(actionRequestDog());
+        return fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(json => dispatch(actionGetImage(json)))
+        .catch(error => dispatch(actionFailedRequest(error)))
+    };
+}
+
+const INITIAL_STATE = {
+    isFetching: false,
+    imagePath: '',
+    error: '',
+}
+
+function reducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case REQUEST_IMAGE:
+            return {
+                ...state,
+                isFetching: true,
+            };
+        case GET_IMAGE:
+            return {
+                ...state,
+                imagePath: action.payload,
+                isFetching: false,
+            }
+        case FAILED_REQUEST:
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false,
+            }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
