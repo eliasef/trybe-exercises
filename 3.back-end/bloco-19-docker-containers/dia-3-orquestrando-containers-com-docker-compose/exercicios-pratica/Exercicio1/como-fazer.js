5a4b9dd0841d6bcd67390f51e2436f1acc09a903
EliasEF <eliasef0305@gmail.com>
2022-10-10 20:05:40 -0300

    feat: add ex01

---
+++ 3.back-end/bloco-19-docker-containers/dia-3-orquestrando-containers-com-docker-compose/exercicios-pratica/Exercicio1/como-fazer.js
@@ -0,0 +1,11 @@
+// cria index.html
+
+// docker run -itd -p 4545:80 --name meu-container -v/home/elias/Mentoria/Exercicio1/:/usr/local/apache2/htdocs/ httpd:2.4
+
+// alterar html
+
+// docker inspect meu-container
+
+// remover container dokcer ps -a
+
+// remover imagem docker images -> docker rmi (id da img)
